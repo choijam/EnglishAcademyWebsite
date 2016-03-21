0686aab65697760f6158f53f5beb7a2d55d204c0
Jihye Choi <jessestarr_1@nate.com>
2016-03-21 03:47:18 +0000

    mp3_2

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
-  #config.fog_provider = 'fog/aws'                        # required
+
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAI3K2SHEUAJQRDBDQ',                        # required
     aws_secret_access_key: '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',                        # required
-    region:                'ap-northeast-2'               # optional, defaults to 'us-east-1'
-  
+    region:                'ap-northeast-2'       # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'highoedae'
+  config.fog_directory  = 'highoedae'                          # required
+
 end
